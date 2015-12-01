6809f29e0e24f13c1aaf1248115cab12e1cc97bb
J. B. Rainsberger <me@jbrains.ca>
2015-12-01 11:38:49 +0100

    Integers now behave like fractions.

---
+++ test/source/ca/jbrains/math/test/Fraction.java
@@ -0,0 +1,17 @@
+package ca.jbrains.math.test;
+
+public class Fraction {
+    private final int integerValue;
+
+    public Fraction(int integerValue) {
+        this.integerValue = integerValue;
+    }
+
+    public Fraction plus(Fraction that) {
+        return new Fraction(this.integerValue + that.integerValue);
+    }
+
+    public int intValue() {
+        return integerValue;
+    }
+}
