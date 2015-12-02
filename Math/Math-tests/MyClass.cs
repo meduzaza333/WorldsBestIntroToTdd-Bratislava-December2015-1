5e34f0c3310f821f1319dca81265a724c8889633
J. B. Rainsberger <me@jbrains.ca>
2015-12-02 13:58:34 +0100

    We can handle n + 0 for all n and 0 + n for all n.

--- Math/Math-tests/MyClass.cs
+++ Math/Math-tests/MyClass.cs
@@ -13,9 +13,9 @@ namespace MathTests
 				this.IntValue = intValue;
 			}
 
-			public Fraction Plus (Fraction fraction)
+			public Fraction Plus (Fraction that)
 			{
-				return this;
+				return (that.IntValue == 0) ? this : that;
 			}
 
 			public int IntValue {
@@ -23,11 +23,11 @@ namespace MathTests
 			}
 		}
 
-		[Test]
-		public void AddIntegerValues(
-			[Values(0, 7)] int sum, 
-			[Values(0, 7)] int addend, 
-			[Values(0, 0)] int augend)
+		[Test, Sequential]
+		public void AddIntegerValues (
+			[Values (0, 7, 4)] int sum, 
+			[Values (0, 7, 0)] int addend, 
+			[Values (0, 0, 4)] int augend)
 		{
 			Assert.AreEqual (sum, new Fraction (addend).Plus (new Fraction (augend)).IntValue);
 		}
