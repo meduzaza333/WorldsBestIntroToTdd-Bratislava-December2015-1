4dff8af471774b321f5b58515df70b1203b71834
J. B. Rainsberger <me@jbrains.ca>
2015-12-02 10:04:10 +0100

    We can now display the price for any barcode, as long as we know the barcode.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -3,6 +3,9 @@ package ca.jbrains.pos.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
@@ -14,6 +17,16 @@ public class SellOneItemTest {
         Assert.assertEquals("EUR 7.95", display.getText());
     }
 
+    @Test
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("EUR 12.50", display.getText());
+    }
+
     public static class Sale {
         private Display display;
 
@@ -22,9 +35,19 @@ public class SellOneItemTest {
         }
 
         public void onBarcode(String barcode) {
-            display.setText("EUR 7.95");
+            final Map<String, String> pricesByBarcode
+                    = new HashMap<String, String>() {{
+                put("12345", "EUR 7.95");
+                put("23456", "EUR 12.50");
+            }};
+
+            final String price = pricesByBarcode.get(barcode);
+            if (price != null) {
+                display.setText(price);
+            }
         }
     }
+
     public static class Display {
         private String text;
 
