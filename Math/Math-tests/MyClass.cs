d351094e29818c4ec3d2787282fa3e12dd0a24ec
J. B. Rainsberger <me@jbrains.ca>
2015-12-02 13:51:34 +0100

    We can add fractions, as long as they add to 0.

---
+++ Math/Math-tests/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MathTests
+{
+	[TestFixture]
+	public class AddFractions
+	{
+		public class Fraction
+		{
+			public Fraction (int intValue)
+			{
+				this.IntValue = intValue;
+			}
+
+			public Fraction Plus (Fraction fraction)
+			{
+				return this;
+			}
+
+			public int IntValue
+			{
+				get;
+			}
+		}
+
+		[Test]
+		public void ZeroPlusZero ()
+		{
+			Assert.AreEqual (0, new Fraction (0).Plus (new Fraction (0)).IntValue);
+		}
+	}
+}
+
